86d530c464faf16258e0d0b8bd1bbecd3c2f7f72
simonha94 <58327915+simonha94@users.noreply.github.com>
2021-07-29 13:23:38 +0200

    Add files via upload

--- examples/graphproppred/mol/main_pyg.py
+++ examples/graphproppred/mol/main_pyg.py
@@ -1,5 +1,5 @@
 import torch
-from torch_geometric.data import DataLoader
+from torch_geometric.data import DataLoader, Data
 import torch.optim as optim
 import torch.nn.functional as F
 from gnn import GNN
@@ -13,20 +13,44 @@ from ogb.graphproppred import PygGraphPropPredDataset, Evaluator
 cls_criterion = torch.nn.BCEWithLogitsLoss()
 reg_criterion = torch.nn.MSELoss()
 
+"""sh 2021 graph project"""
+
+def get_idx_split(split_type=None):
+    import pandas as pd
+
+    if split_type is None:
+        split_type = self.meta_info['split']
+
+    #path = 'dataset/ogbg_Cora/meta_dict.pt'
+
+    return torch.load('/home/simon/Documents/repos/ogb/examples/graphproppred/mol/dataset/ogbg_Cora/split/random/split_dict.pt')
+
+
 def train(model, device, loader, optimizer, task_type):
     model.train()
 
     for step, batch in enumerate(tqdm(loader, desc="Iteration")):
         batch = batch.to(device)
-
-        if batch.x.shape[0] == 1 or batch.batch[-1] == 0:
-            pass
-        else:
+        try:
+            if batch.x.shape[0] == 1 or batch.batch[-1] == 0: # sh 2021
+                pass
+            else:
+                pred = model(batch)
+                optimizer.zero_grad()
+                ## ignore nan targets (unlabeled) when computing training loss.
+                is_labeled = batch.y == batch.y
+                if "classification" in task_type:
+                    loss = cls_criterion(pred.to(torch.float32)[is_labeled], batch.y.to(torch.float32)[is_labeled])
+                else:
+                    loss = reg_criterion(pred.to(torch.float32)[is_labeled], batch.y.to(torch.float32)[is_labeled])
+                loss.backward()
+                optimizer.step()
+        except: # sh 2021
             pred = model(batch)
             optimizer.zero_grad()
             ## ignore nan targets (unlabeled) when computing training loss.
             is_labeled = batch.y == batch.y
-            if "classification" in task_type: 
+            if "classification" in task_type:
                 loss = cls_criterion(pred.to(torch.float32)[is_labeled], batch.y.to(torch.float32)[is_labeled])
             else:
                 loss = reg_criterion(pred.to(torch.float32)[is_labeled], batch.y.to(torch.float32)[is_labeled])
@@ -49,10 +73,8 @@ def eval(model, device, loader, evaluator):
 
             y_true.append(batch.y.view(pred.shape).detach().cpu())
             y_pred.append(pred.detach().cpu())
-
     y_true = torch.cat(y_true, dim = 0).numpy()
     y_pred = torch.cat(y_pred, dim = 0).numpy()
-
     input_dict = {"y_true": y_true, "y_pred": y_pred}
 
     return evaluator.eval(input_dict)
@@ -91,6 +113,7 @@ def main():
     device = torch.device("cuda:" + str(args.device)) if torch.cuda.is_available() else torch.device("cpu")
 
     ### automatic dataloading and splitting
+
     dataset = PygGraphPropPredDataset(name = args.dataset)
 
     """here preprocess for sgc if needed!, sh graph project 2021"""
@@ -106,26 +129,33 @@ def main():
         dataset.data.x = dataset.data.x[:,:2]
         dataset.data.edge_attr = dataset.data.edge_attr[:,:2]
 
+
     split_idx = dataset.get_idx_split()
+
 
     ### automatic evaluator. takes dataset name as input
     evaluator = Evaluator(args.dataset)
+
 
     train_loader = DataLoader(dataset[split_idx["train"]], batch_size=args.batch_size, shuffle=True, num_workers = args.num_workers)
     valid_loader = DataLoader(dataset[split_idx["valid"]], batch_size=args.batch_size, shuffle=False, num_workers = args.num_workers)
     test_loader = DataLoader(dataset[split_idx["test"]], batch_size=args.batch_size, shuffle=False, num_workers = args.num_workers)
 
+
+    num_tasks = dataset.num_tasks
+    task_type = dataset.task_type
+
     if args.gnn == 'gin':
-        model = GNN(gnn_type = 'gin', num_tasks = dataset.num_tasks, num_layer = args.num_layer, emb_dim = args.emb_dim, drop_ratio = args.drop_ratio, virtual_node = False).to(device)
+        model = GNN(gnn_type = 'gin', num_tasks = num_tasks, num_layer = args.num_layer, emb_dim = args.emb_dim, drop_ratio = args.drop_ratio, virtual_node = False).to(device)
     elif args.gnn == 'gin-virtual':
-        model = GNN(gnn_type = 'gin', num_tasks = dataset.num_tasks, num_layer = args.num_layer, emb_dim = args.emb_dim, drop_ratio = args.drop_ratio, virtual_node = True).to(device)
+        model = GNN(gnn_type = 'gin', num_tasks = num_tasks, num_layer = args.num_layer, emb_dim = args.emb_dim, drop_ratio = args.drop_ratio, virtual_node = True).to(device)
     elif args.gnn == 'gcn':
-        model = GNN(gnn_type = 'gcn', num_tasks = dataset.num_tasks, num_layer = args.num_layer, emb_dim = args.emb_dim, drop_ratio = args.drop_ratio, virtual_node = False).to(device)
+        model = GNN(gnn_type = 'gcn', num_tasks = num_tasks, num_layer = args.num_layer, emb_dim = args.emb_dim, drop_ratio = args.drop_ratio, virtual_node = False).to(device)
     elif args.gnn == 'gcn-virtual':
-        model = GNN(gnn_type = 'gcn', num_tasks = dataset.num_tasks, num_layer = args.num_layer, emb_dim = args.emb_dim, drop_ratio = args.drop_ratio, virtual_node = True).to(device)
+        model = GNN(gnn_type = 'gcn', num_tasks = num_tasks, num_layer = args.num_layer, emb_dim = args.emb_dim, drop_ratio = args.drop_ratio, virtual_node = True).to(device)
     elif args.gnn == 'sgc':
         """sh, graph project 2021"""
-        model = GNN(gnn_type = 'sgc', num_tasks = dataset.num_tasks, num_layer = args.num_layer, emb_dim = args.emb_dim, drop_ratio = args.drop_ratio, virtual_node = False, normalization=args.adj_norm).to(device)
+        model = GNN(gnn_type = 'sgc', num_tasks = num_tasks, num_layer = args.num_layer, emb_dim = args.emb_dim, drop_ratio = args.drop_ratio, virtual_node = False, normalization=args.adj_norm).to(device)
     else:
         raise ValueError('Invalid GNN type')
 
@@ -138,7 +168,7 @@ def main():
     for epoch in range(1, args.epochs + 1):
         print("=====Epoch {}".format(epoch))
         print('Training...')
-        train(model, device, train_loader, optimizer, dataset.task_type)
+        train(model, device, train_loader, optimizer, task_type) # sh 2021
 
         print('Evaluating...')
         train_perf = eval(model, device, train_loader, evaluator)
@@ -151,7 +181,7 @@ def main():
         valid_curve.append(valid_perf[dataset.eval_metric])
         test_curve.append(test_perf[dataset.eval_metric])
 
-    if 'classification' in dataset.task_type:
+    if 'classification' in task_type:
         best_val_epoch = np.argmax(np.array(valid_curve))
         best_train = max(train_curve)
     else:
